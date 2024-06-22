7bb3a4f997060acee46ec852e429bb45093ec4c4
OYETADE Segun John <johnwellacademy@gmail.com>
2024-06-22 14:08:28 +0100

    Update universe.sql

--- universe.sql
+++ universe.sql
@@ -1,3 +1,4 @@
+--curl -sL https://raw.githubusercontent.com/oyetadesegun/psql-Celestial-Bodies/main/universe.sql | psql --username=freecodecamp --dbname=postgres
 CREATE TABLE IF NOT EXISTS checke (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
